247ffd210dcc19e654ad8b5ddd3dd5b4505b1906
namankumarsaxena <namansaxenackc@gmail.com>
2026-06-26 07:33:03 +0000

    Added backend, websocket

--- packages/db/index.ts
+++ packages/db/index.ts
@@ -1,11 +1,12 @@
+import "dotenv/config";
+
 import { PrismaClient } from "./generated/prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
-
+console.log(process.env.DATABASE_URL);
 const adapter = new PrismaPg({
-  connectionString: process.env.DATABASE_URL,
+  connectionString: process.env.DATABASE_URL!,
 });
 
-
 export const prismaClient = new PrismaClient({
-    adapter,
-  });
+  adapter,
+});
